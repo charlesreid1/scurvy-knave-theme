5c9fe5b23c2e399fd92a2f747004c1f45d801ceb
Charles Reid <charlesreid1@gmail.com>
2018-04-14 16:23:52 -0700

    add pelican to source branch

---
+++ pelican/pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'charlesreid1'
+SITENAME = u'scurvy knave pelican theme'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'America/Los_Angeles'
+DEFAULT_LANG = u'en'
+
+# --------------8<---------------------
+
+THEME = 'scurvy-knave-theme'
+H1COLOR = "#c0deed"
+ACOLOR = "#00aced"
+AHOVERCOLOR = "#0084b4"
+TEMPLATE_PAGES = {
+    'custom.css' : 'custom.css'
+}
+
+# ---------------------------
+
+SITE_TITLE = "scurvy knave pelican theme"
+SITE_DESCRIPTION = "a simple one-color, one-page pelican theme"
+
+# ---------------------------
+
+ABOUT_TITLE = "about scurvy knave"
+ABOUT_DESCRIPTION = ""
+
+# ---------------------------
+
+LINKS_TITLE = "links on bots.charlesreid1.com"
+LINKS_DESCRIPTION = ""
+LINKS_BUTTONS = ""
+
+# ---------------------------
+
+CONTACT_TITLE = "Contact"
+CONTACT_DESCRIPTION = "<p>Get in touch!</p>"
+
